0d6fad2c79c891dc39bf8262dc921318ba47874a
KangWon LEE <kangwon@gmail.com>
2025-10-22 13:13:51 +0900

    add comments to limit `strcpy` length

--- ch05/ch05/ex13.cpp
+++ ch05/ch05/ex13.cpp
@@ -6,6 +6,7 @@ int main() {
     char s2[100] = {0};
 
     strcpy(s2, s1);  // s2에 s1의 내용물을 복사한다.
+    // s 보다 짧은 문자열만 복사하도록 제한 바랍니다.
     printf("s1: %s\n", s1);
     printf("s2: %s\n", s2);
 }
--- ch05/ch05/ex14.cpp
+++ ch05/ch05/ex14.cpp
@@ -5,5 +5,6 @@ int main() {
     char s[100] = {0};
 
     strcpy(s, "hello");  // s에 "hello"라는 상수형 문자열을 복사한다.
+    // s 보다 짧은 문자열만 복사하도록 제한 바랍니다.
     printf("s: %s\n", s);
 }
