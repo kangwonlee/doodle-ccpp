97d537c9c444ffdb9187eb7f88dfc86cf396d196
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2025-10-22 11:57:58 +0900

    add comment of `>> 2` instead of `/ sizeof(int)`
    Added comment explaining size calculation for array.

--- ch05/ch05/ex03.cpp
+++ ch05/ch05/ex03.cpp
@@ -4,6 +4,8 @@ int main() {
     int arr[5] = { 1, 3, 5, 7, 9 };
 
     const int n = sizeof(arr) / sizeof(arr[0]);
+    // if `int`
+    //  const int n = sizeof(arr) >> 2;
 
     for (int i = 0; i < n; i++) {
         printf("%d\n", arr[i]);
