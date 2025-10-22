a39a84f1afeaca09f695ee6deae05ac0b32263b3
KangWon LEE <kangwon@gmail.com>
2025-10-22 13:01:01 +0900

    clarify size of the `int`

--- ch05/ch05/ex03.cpp
+++ ch05/ch05/ex03.cpp
@@ -4,7 +4,7 @@ int main() {
     int arr[5] = { 1, 3, 5, 7, 9 };
 
     const int n = sizeof(arr) / sizeof(arr[0]);
-    // if `int`
+    // if `int` is 4 bytes,
     //  const int n = sizeof(arr) >> 2;
 
     for (int i = 0; i < n; i++) {
