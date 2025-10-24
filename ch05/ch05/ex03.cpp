5091fa718d3c3886af29c438df02d9158fca3041
Kangwon Lee (Education) <kangwonlee@users.noreply.github.com>
2025-10-24 12:02:00 +0900

    process of `>> 2` in comments
    Added comments explaining the calculation of array size.

--- ch05/ch05/ex03.cpp
+++ ch05/ch05/ex03.cpp
@@ -6,6 +6,15 @@ int main() {
     const int n = sizeof(arr) / sizeof(arr[0]);
     // if `int` is 4 bytes,
     //  const int n = sizeof(arr) >> 2;
+    // 20(10)
+    // = 16(10) + 4(10)
+    // = 2**4(10) + 2**2(10)
+    // = 10000(2) + 100(2)
+    // = 10100(2)
+    // 20(10) >> 2(10)
+    // = 10100(2) >> 2(10)
+    // =   101(2)
+    // = 5(10)
 
     for (int i = 0; i < n; i++) {
         printf("%d\n", arr[i]);
