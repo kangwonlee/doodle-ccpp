08506f2ec6416a257d18ada99c3dc98a7227d076
KangWon LEE <kangwon@gmail.com>
2025-10-27 10:59:51 +0900

    ch05 / ex24 : print in `%ld`

--- ch05/ch05/ex24.cpp
+++ ch05/ch05/ex24.cpp
@@ -3,15 +3,17 @@
 
 int main() {
 
-    int a = 10;
-    int* ptr_a = &a;
+    const int a = 10;
+    const int* ptr_a = &a;
 
-    printf("ptr_a = %d\n", ptr_a);
-
-    ptr_a ++;
-    printf("ptr_a ++ : %d\n", ptr_a);
+    printf("ptr_a    = %ld\n", (long) ptr_a);
 
     ptr_a ++;
-    printf("ptr_a ++ : %d\n", ptr_a);
-    // printf("%d\n", *ptr_a); 는 어떤 값을 화면에 표시?
+    printf("ptr_a ++ : %ld\n", (long) ptr_a);
+
+    ptr_a ++;
+    printf("ptr_a ++ : %ld\n", (long) ptr_a);
+
+    // printf("%d\n", *ptr_a);
+    // 위 행의 출력 결과를 예상할 수 있는가?
 }
