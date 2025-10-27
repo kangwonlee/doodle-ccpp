52db55f301d09afca47d08a7e5259d2a82eca939
KangWon LEE <kangwon@gmail.com>
2025-10-27 10:15:54 +0900

    c050 / ex18 : initialize `ptr_a` with `NULL`

--- ch05/ch05/ex18.cpp
+++ ch05/ch05/ex18.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    // 정수 변수 a를 정의하고 10으로 초기화
     int a = 10;
-    int* ptr_a;  // 포인터 선언
-    ptr_a = &a;  // ptr_a에 a의 주솟값 저장
+    // 포인터 선언. (현재는 가리키는 것에 별 뜻 없다는 의미로) NULL 값으로 초기화
+    int* ptr_a = NULL;
+    // ptr_a에 a의 주솟값 저장
+    ptr_a = &a;
 
     printf("a의 값 : %d\n", a);
     printf("a의 주솟값 : %d\n", &a);
