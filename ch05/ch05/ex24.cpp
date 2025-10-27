35a65434f490cce92de18d601e8e2a6be1fba2a7
KangWon LEE <kangwon@gmail.com>
2025-10-27 10:43:54 +0900

    ch05 / ex24 : `printf("%d\n", *ptr_a);` ?

--- ch05/ch05/ex24.cpp
+++ ch05/ch05/ex24.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+
 int main() {
+
     int a = 10;
     int* ptr_a = &a;
 
     printf("ptr_a = %d\n", ptr_a);
+
     ptr_a ++;
     printf("ptr_a ++ : %d\n", ptr_a);
+
     ptr_a ++;
     printf("ptr_a ++ : %d\n", ptr_a);
+    // printf("%d\n", *ptr_a); 는 어떤 값을 화면에 표시?
 }
