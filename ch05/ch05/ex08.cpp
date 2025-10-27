dd2315636dd498e19360abbd1ba09448a70d3f57
KangWon LEE <kangwon@gmail.com>
2025-10-27 10:08:49 +0900

    feature : compare s and &(s[0])

--- ch05/ch05/ex08.cpp
+++ ch05/ch05/ex08.cpp
@@ -10,8 +10,9 @@ int main() {
     for (int i = 0; i < n; i++) {
       printf("s[%2d] = %c (ASCII %04x)\n", i, (s[i]), (int) s[i]);
     }
+
     puts("Null Terminated String");
-    //printf("(s[0]) = c\n", (s[0]));
-    //printf("s = %x\n", (int) s);
-    //printf("&(s[0]) = %x\n", (int) &(s[0]));
+
+    printf("s       = %x\n", (long int) s);
+    printf("&(s[0]) = %x\n", (long int) &(s[0]));
 }
