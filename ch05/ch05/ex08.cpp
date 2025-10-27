853b4764f907aa505f16b74d304aa4619b653eb7
KangWon LEE <kangwon@gmail.com>
2025-10-27 10:04:50 +0900

    feature : null terminated string

--- ch05/ch05/ex08.cpp
+++ ch05/ch05/ex08.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    char s[] = "Hello, world!";
+    const char s[] = "Hello, world !";
+    //                12345678901234
+    const int n = sizeof(s) / sizeof(s[0]);
+
+    printf("n = %d\n", n);
     printf("%s\n", s);  // 문자열 전체가 한 번에 출력된다.
+    for (int i = 0; i < n; i++) {
+      printf("s[%2d] = %c (ASCII %04x)\n", i, (s[i]), (int) s[i]);
+    }
+    puts("Null Terminated String");
+    //printf("(s[0]) = c\n", (s[0]));
+    //printf("s = %x\n", (int) s);
+    //printf("&(s[0]) = %x\n", (int) &(s[0]));
 }
