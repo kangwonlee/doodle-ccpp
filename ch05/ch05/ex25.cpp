6ec079115809b7171e2e8cbaf634bfda820d6db9
KangWon LEE <kangwon@gmail.com>
2025-10-28 11:19:02 +0900

    ch05 / ex25 : const

--- ch05/ch05/ex25.cpp
+++ ch05/ch05/ex25.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    const int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
     for (int i = 0; i < 10; i++) {
         printf("arr[%d]ÀÇ ÁÖ¼Ò : %d\n", i, &arr[i]);
