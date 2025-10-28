3f646ff3d51f20d943e31b1a7cf3eb7651a011c5
KangWon LEE <kangwon@gmail.com>
2025-10-28 11:28:58 +0900

    ch05 / ex30 : const

--- ch05/ch05/ex30.cpp
+++ ch05/ch05/ex30.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int arr[3] = { 1, 2, 3 };
-    int(*p_arr)[3] = &arr;
+    const int arr[3] = { 1, 2, 3 };
+    const int(*p_arr)[3] = &arr;
 
     for (int i = 0; i < 3; i++) {
         printf("%d\n", (*p_arr)[i]);
