e122673bdd17e094aa1aeaee5127bb7ada9db1c8
KangWon LEE <kangwon@gmail.com>
2025-10-28 10:29:00 +0900

    ch05 / ex22 : print `*ptr_a`

--- ch05/ch05/ex22.cpp
+++ ch05/ch05/ex22.cpp
@@ -5,11 +5,12 @@ int main() {
     const int* ptr_a = &a;
     const int** ptr_ptr_a = &ptr_a;  // 더블 포인터 선언
 
-    printf("1. a           = %10ld\n", (long int) a);
+    printf("1. a           = %10d\n", a);
     printf("2. &a          = %10ld\n", (long int) &a);
     printf("3. ptr_a       = %10ld\n", (long int) ptr_a);
-    printf("4. &ptr_a      = %10ld\n", (long int) &ptr_a);
-    printf("5. ptr_ptr_a   = %10ld\n", (long int) ptr_ptr_a);
-    printf("6. *ptr_ptr_a  = %10ld\n", (long int) *ptr_ptr_a);
-    printf("7. **ptr_ptr_a = %10ld\n", (long int) **ptr_ptr_a);
+    printf("4. *ptr_a      = %10d\n", *ptr_a);
+    printf("5. &ptr_a      = %10ld\n", (long int) &ptr_a);
+    printf("6. ptr_ptr_a   = %10ld\n", (long int) ptr_ptr_a);
+    printf("7. *ptr_ptr_a  = %10ld\n", (long int) *ptr_ptr_a);
+    printf("8. **ptr_ptr_a = %10d\n", *(*ptr_ptr_a));
 }
