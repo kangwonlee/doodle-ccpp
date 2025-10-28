29b95693b8658c07f63bbe46e7eb2138851e0cab
KangWon LEE <kangwon@gmail.com>
2025-10-28 11:30:50 +0900

    ch05 / ex33 : const

--- ch05/ch05/ex33.cpp
+++ ch05/ch05/ex33.cpp
@@ -2,12 +2,12 @@
 #include "ex33.h"
 
 int main() {
-    int arr[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+    const int arr[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
 
-    int (*last_row)[4] = arr +3;
+    const int (*last_row)[4] = arr +3;
 
-    for (int (*pi)[4] = arr; pi < last_row; pi++) {
-        for (int* pj = *pi; pj < *pi + 4; pj++) {
+    for (const int (*pi)[4] = arr; pi < last_row; pi++) {
+        for (const int* pj = *pi; pj < *pi + 4; pj++) {
             printf("%4d", *pj);
         }
         printf("\n");  // 한 행을 출력한 후, 줄 바꿈
