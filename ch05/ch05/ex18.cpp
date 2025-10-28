67fc3040061315e0a5ed23ed873a409b924aa6cf
KangWon LEE <kangwon@gmail.com>
2025-10-28 10:28:31 +0900

    ch05 / ex18 : `a=7` for `%x`

--- ch05/ch05/ex18.cpp
+++ ch05/ch05/ex18.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     // 정수 변수 a를 정의하고 10으로 초기화
-    const int a = 10;
+    const int a = 7;
     // 포인터 선언. (현재는 가리키는 것에 별 뜻 없다는 의미로) NULL 값으로 초기화
     const int* ptr_a = NULL;
     // ptr_a에 a의 주솟값 저장
