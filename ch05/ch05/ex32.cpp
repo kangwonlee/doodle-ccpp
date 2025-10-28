80b4092f5af196e5682340a9602f145bbe66bba8
KangWon LEE <kangwon@gmail.com>
2025-10-28 11:30:25 +0900

    ch05 / ex31 : const

--- ch05/ch05/ex32.cpp
+++ ch05/ch05/ex32.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int arr[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
-    int(*p_row)[4] = &arr[1];
+    const int arr[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+    const int(*p_row)[4] = &arr[1];
 
     for (int i = 0; i < 4; i++) {
         printf("%d ", (*p_row)[i]);
