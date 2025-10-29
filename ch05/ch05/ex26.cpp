3d9737ba63c713edce4d7771609cabae886b6456
KangWon LEE <kangwon@gmail.com>
2025-10-29 12:17:47 +0900

    ch05 / ex26 : `%ld`

--- ch05/ch05/ex26.cpp
+++ ch05/ch05/ex26.cpp
@@ -3,6 +3,6 @@
 int main() {
     int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    printf("arr의 값 : %d\n", arr);
-    printf("&arr[0]의 값 : %d\n", &arr[0]);
+    printf("arr의 값 : %ld\n", (long) arr);
+    printf("&arr[0]의 값 : %ld\n", (long) &arr[0]);
 }
