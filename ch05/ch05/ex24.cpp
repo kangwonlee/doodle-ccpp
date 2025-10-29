f7e4dec7bebc5389ada4bbe52cd1032d3e7322ee
KangWon LEE <kangwon@gmail.com>
2025-10-29 12:10:24 +0900

    ch05 / ex24 : little / Big Endian check

--- ch05/ch05/ex24.cpp
+++ ch05/ch05/ex24.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 
-    const int a = 10;
+    const int a = 0x10203040;
     const int* ptr_a = &a;
 
     printf("ptr_a    = %ld\n", (long) ptr_a);
@@ -16,4 +16,10 @@ int main() {
 
     // printf("%d\n", *ptr_a);
     // 위 행의 출력 결과를 예상할 수 있는가?
+
+    const char* p = (char *) &a;
+    printf("%ld\t%x\n", (long) p+0, (int) *(p+0));
+    printf("%ld\t%x\n", (long) p+1, (int) *(p+1));
+    printf("%ld\t%x\n", (long) p+2, (int) *(p+2));
+    printf("%ld\t%x\n", (long) p+3, (int) *(p+3));
 }
